02bc1cbfe7734ec34eeaa070d1c711f3fa46ddd7
Mai <catty_chan16@yahoo.com>
2015-03-14 23:53:49 +0100

    show als formular-form fertig

--- app/models/kunde.rb
+++ app/models/kunde.rb
@@ -1,5 +1,9 @@
 class Kunde < ActiveRecord::Base
 	has_many :anmeldungs
+	validates :gruppe, :anrede, :nachname, :vorname, :geburtsdatum, :geburtsort, :adresse, :plz, :stadt, presence: true
+	validates_inclusion_of :tsp_versicherungskunde, :in => [true, false]
+	validates_length_of :plz, :maximum => 5
+
 
 	def full_name
     "ID: " + self.id.to_s + ". " + self.nachname + ", " + self.vorname
